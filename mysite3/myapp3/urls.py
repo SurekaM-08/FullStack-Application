15a11fd79505bb1b2286986118273ce037990fa4
Sureka M <surekam2006@gmail.com>
2025-12-29 15:08:33 +0530

    Delete mysite3 directory

--- mysite3/myapp3/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('register/',views.reg,name='register'),
-    path('login/',views.login,name='login'),
-    path('get/',views.get_data,name='get'),
-    path('del/',views.get_users,name='del'),
-    path('update/',views.update_user,name='update'),
-]
